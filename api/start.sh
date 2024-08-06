b1198c1cd304efc30ff808a8eabb8eb0ec306fcf
Katie Harris <katie.harris2397@gmail.com>
2024-08-06 16:44:33 -0400

    More updates to deployment

--- api/start.sh
+++ api/start.sh
@@ -2,6 +2,6 @@
 
 python3 manage.py migrate
 python3 manage.py populatedb
-python3 manage.py createsuperuser --email kitbear23@gmail.com --noinput
+DJANGO_SUPERUSER_PASSWORD=admin python3 manage.py createsuperuser --email=admin@admin.com --noinput
 
 gunicorn --bind :8000 --workers 4 --worker-class saleor.asgi.gunicorn_worker.UvicornWorker saleor.asgi:application
