5ddc348eb240130c47fb10ce69d63097679e8602
Katie Harris <katie.harris2397@gmail.com>
2024-08-06 15:49:41 -0400

    update api start script to actually run gunicorn and update deployment yaml

--- api/start.sh
+++ api/start.sh
@@ -2,4 +2,6 @@
 
 python3 manage.py migrate
 python3 manage.py populatedb
-python3 manage.py createsuperuser --noinput --username admin --email setivol276@almaxen.com --password admin
+python3 manage.py createsuperuser --email kitbear23@gmail.com --noinput
+
+gunicorn --bind :8000 --workers 4 --worker-class saleor.asgi.gunicorn_worker.UvicornWorker saleor.asgi:application
