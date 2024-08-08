3c8e8d4a0f202819262e61a85af4af5e48608b1e
Katie Harris <katie.harris2397@gmail.com>
2024-08-08 16:13:18 -0400

    Add k8s yamls used for template

--- api/start.sh
+++ api/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 python3 manage.py migrate
-python3 manage.py populatedb --createsuperuser
+#python3 manage.py populatedb --createsuperuser
 #DJANGO_SUPERUSER_PASSWORD=admin python3 manage.py createsuperuser --email=admin@admin.com --noinput
 
 gunicorn --bind :8000 --workers 4 --worker-class saleor.asgi.gunicorn_worker.UvicornWorker saleor.asgi:application
